b448f4f82287559e8fb8a0017478f4eed944f2ad
Matt Newcomer <mattn@vt.edu>
2022-06-22 16:53:47 -0400

    Comments code, changes defaults, removes images

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ LUMINOSITY_COEFF_ARRAY = [0.21, 0.72, 0.07]
 BRIGHTNESS_CHAR_STRING = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 BRIGHTNESS_CHAR_STRING_LENGTH = len(BRIGHTNESS_CHAR_STRING)
 OUTPUT_FILE_TYPE = ".txt"
-BASE_HEIGHT = 120
+BASE_HEIGHT = 60
 
 # Given a path to an image file, return the path in a form where POST_PROCESS_NAME_APPEND is inserted before the final period (which determines the file type
 
@@ -29,6 +29,8 @@ def compute_dot(v1: list, v2: list) -> float:
         sum += v1[index] * v2[index]
 
     return sum
+
+# Scales an image to have a constate height of BASE_HEIGHT so the resulting file isn't insanely large but also still understandable.
 
 
 def normalize_image_dimensions(width: int, height: int) -> tuple:
@@ -53,6 +55,7 @@ if __name__ == "__main__":
             "Invoke using: python main.py {paths to image files}", file=sys.stderr)
         exit(1)
 
+    # For every file after the invocation
     for infile in sys.argv[1:]:
         try:
             with Image.open(infile) as image:
@@ -61,30 +64,42 @@ if __name__ == "__main__":
                 print(
                     f"Loaded {infile} with size {image_width}x{image_height}")
 
-                new_dimensions = normalize_image_dimensions(image_width, image_height)
+                #Scale the image down
+                new_dimensions = normalize_image_dimensions(
+                    image_width, image_height)
                 resized = image.resize(new_dimensions)
+
+                #Update the width and height to that of the resized image
                 image_width, image_height = resized.size
 
                 print(
                     f"Resized to {image_width}x{image_height}")
 
+                #Load the pixels from the resized image 
                 pixels = resized.load()
 
+                #Remove the extension from the original image, add the modified tag and reappend the type
                 try:
                     f = open(strip_file_type(infile), "w")
                 except:
                     continue
 
+                #Loop through the rows then columns of the image (like in a book)
                 for y in range(image_height):
+
+                    #Buffer the chars of the current line
                     buffer = ""
                     for x in range(image_width):
+
+                        #Get the RGB values at that pixel
                         pixel = pixels[x, y]
                         luminosity = compute_dot(pixel, LUMINOSITY_COEFF_ARRAY)
 
                         # Normalize brightness model then map across string
                         brightness_string_index = int(
                             (luminosity/255.0) * (BRIGHTNESS_CHAR_STRING_LENGTH - 1))
-
+                        
+                        #Append to the buffer 2 of the selected characters (to preserve the ratio)
                         buffer += (
                             BRIGHTNESS_CHAR_STRING[brightness_string_index]*2)
                     f.write(buffer + "\n")
---
+++ readme.md
@@ -0,0 +1,13 @@
+# ASCII Art
+
+A simple python script to ASCII-ify images. 
+
+Works best with high contrast images.
+
+Install dependencies using:
+
+`pip3 install -r requirements.txt`
+
+Invoke using:
+
+`python3 main.py [Paths to images]`
